473b9e6d8cd938f6f57c293e42474ee38062e5ba
OleksandrVaniuta <vaniutasa@gmail.com>
2023-04-01 11:39:21 +0300

    внесли зміни

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Bytton';
 import Loader from './Loader/Loader';
 import css from './App.module.css';
+import photosAPI from '../secvices/photos-api';
 
 class App extends Component {
   state = {
@@ -14,27 +15,22 @@ class App extends Component {
     error: null,
   };
 
-  HanleSubmit = e => {
+  hanleSubmit = e => {
     e.preventDefault();
     const inputValie = e.target.elements.input.value;
     if (inputValie.trim() === '') {
       return;
     }
-    this.setState({ value: inputValie });
+    this.setState({ value: inputValie, page: 1, cards: [], error: null });
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { value, page } = this.state;
-    if (value !== prevState.value) {
-      this.setState({ page: 1, cards: [], error: null });
-    }
 
     if (value !== prevState.value || page !== prevState.page) {
       this.setState({ loading: true });
-      fetch(
-        `https://pixabay.com/api/?q=${this.state.value}&page=${this.state.page}&key=33763278-a8135cc0e5b2d03eb2d8094e4&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(response => response.json())
+      photosAPI
+        .FetchPhotos(value, page)
         .then(res => {
           if (res.hits.length === 0) {
             return Promise.reject(new Error());
@@ -58,7 +54,7 @@ class App extends Component {
     const { cards, loading, error } = this.state;
     return (
       <div className={css.App}>
-        <Searchbar onSubmit={this.HanleSubmit} />
+        <Searchbar onSubmit={this.hanleSubmit} />
         {error && <h2>Something go wrong, please try again with new value!</h2>}
         {cards.length > 0 && <ImageGallery cards={this.state.cards} />}
         {cards.length > 0 && !loading && (
